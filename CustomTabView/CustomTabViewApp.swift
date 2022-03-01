b7691917a98671bcaeca493e08ef0aef429f4d8f
MaksBelenko <maksim.belenko@gmail.com>
2022-03-01 20:16:38 +0000

    Initial Commit

---
+++ CustomTabView/CustomTabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabViewApp.swift
+//  CustomTabView
+//
+//  Created by Maksim on 01/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
